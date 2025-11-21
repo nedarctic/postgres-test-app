23409e34b643026c2cfff749dbdae43d61b7eec1
justkimtai <justkimtai@gmail.com>
2025-11-21 17:53:04 +0300

    added posts app

--- core/views.py
+++ core/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def posts(request):
+    if request.method == 'POST':
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # GET all posts
+    all_posts = Post.objects.all()
+    serializer = PostSerializer(all_posts, many=True)
+    return Response(serializer.data)
