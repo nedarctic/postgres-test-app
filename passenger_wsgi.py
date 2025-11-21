c84e03fe1bef0fcdb1489c9ce47f339a08e362fd
justkimtai <justkimtai@gmail.com>
2025-11-21 18:23:24 +0300

    changed contents to work for app on server

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,9 +1,17 @@
-import imp
-import os
 import sys
+import os
 
+# 1️⃣ Add your project folder to the Python path
+sys.path.insert(0, '/home/justuski/repositories/postgres-test-app')
 
-sys.path.insert(0, os.path.dirname(__file__))
+# 2️⃣ Set Django settings module
+os.environ['DJANGO_SETTINGS_MODULE'] = 'postgres_test_project.settings'
 
-wsgi = imp.load_source('wsgi', 'passenger_wsgi.py')
-application = wsgi.application
+# 3️⃣ Activate your virtual environment
+activate_env = '/home/justuski/virtualenv/repositories/postgres-test-app/3.10/bin/activate_this.py'
+with open(activate_env) as file_:
+    exec(file_.read(), dict(__file__=activate_env))
+
+# 4️⃣ Get Django WSGI application
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
